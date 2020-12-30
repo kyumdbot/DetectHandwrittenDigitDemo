2258d39a05e074a9bc3782e2652c8c290bc448f5
kyumdbot <kyumd001@gmail.com>
2020-12-30 20:51:51 +0800

    add appDelegate

--- DetectHandwrittenDigitDemo/DetectHandwrittenDigitDemoApp.swift
+++ DetectHandwrittenDigitDemo/DetectHandwrittenDigitDemoApp.swift
@@ -9,9 +9,17 @@ import SwiftUI
 
 @main
 struct DetectHandwrittenDigitDemoApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
     }
 }
